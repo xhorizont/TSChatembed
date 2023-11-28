57c747cd26da3d7918d17ab79f59df15dd9cac6a
John A. Jazbec <johnny@xhorizont.com>
2023-11-28 19:59:43 +0100

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Telekom Slovenije</span>
       </a>
     </span>
   )
